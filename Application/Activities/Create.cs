4e50dacc2ab1fa06197e66c8afe532b8b6a5222e
Jakub Kubasiak <xjk@pfa.dk>
2020-07-09 15:54:38 +0200

    end of section 4

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistance;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class CreateCommand : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
+
+        public class Handler : IRequestHandler<CreateCommand>
+        {
+            private readonly DataContext context;
+            public Handler(DataContext context)
+            {
+                this.context = context;
+
+            }
+            public async Task<Unit> Handle(CreateCommand request, CancellationToken cancellationToken)
+            {
+                var activity = new Activity() 
+                {
+                    Id = request.Id,
+                    Category = request.Category,
+                    City = request.City,
+                    Date = request.Date,
+                    Description = request.Description,
+                    Title = request.Title,
+                    Venue = request.Venue
+                };
+                
+                this.context.Activities.Add(activity);
+                var success = await this.context.SaveChangesAsync() > 0;
+
+                if(success) 
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Persistance;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class DeleteCommand : IRequest
+                {
+                    public Guid Id { get; set; }
+                }
+        
+                public class Handler : IRequestHandler<DeleteCommand>
+                {
+                    private readonly DataContext context;
+                    public Handler(DataContext context)
+                    {
+                        this.context = context;
+        
+                    }
+                    public async Task<Unit> Handle(DeleteCommand request, CancellationToken cancellationToken)
+                    {
+                        var activity = await this.context.Activities.FindAsync(request.Id);
+                        if(activity == null)
+                        {
+                            throw new Exception("Could not find activity");
+                        }
+
+                        this.context.Remove(activity);
+                        var success = await this.context.SaveChangesAsync() > 0;
+        
+                        if(success) 
+                        {
+                            return Unit.Value;
+                        }
+        
+                        throw new Exception("Problem saving changes");
+                    }
+                }
+    }
+}
