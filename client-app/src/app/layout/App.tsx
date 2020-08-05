7ee7b1b5a4ac0ffaed17eb3664ef4bf6a46da3e6
Jakub Kubasiak <xjk@pfa.dk>
2020-08-05 15:26:36 +0200

    end of section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Fragment } from 'react';
 import { Container } from 'semantic-ui-react'
 import NavBar from '../../features/NavBar';
@@ -11,10 +11,31 @@ import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import NotFound from './NotFound';
 import {ToastContainer} from 'react-toastify';
+import LoginForm from '../../features/user/LoginForm';
+import { RootStoreContext } from '../stores/rootStore';
+import { LoadingComponent } from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { setAppLoaded, token, appLoaded } = rootStore.commonStore;
+  const { getUser} = rootStore.userStore;
+
+  useEffect(() => {
+    if(token) {
+      getUser().finally(() => {
+        setAppLoaded()
+      });
+    } else {
+      setAppLoaded();
+    }
+  }, [getUser, setAppLoaded, token])
+
+  if(!appLoaded) return <LoadingComponent content='Loading app...' />
+
   return (
     <Fragment>
+      <ModalContainer />
       <ToastContainer position='bottom-right'></ToastContainer>
       <Route exact path='/' component={HomePage} />
       <Route path={'/(.+)'} render={() => (
@@ -26,6 +47,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
               <Route exact path='/activities' component={ActivityDashboard} />
               <Route path='/activities/:id' component={ActivityDetails} />
               <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Route path='/login' component={LoginForm} />
               <Route component={NotFound} />
             </Switch>
           </Container>
---
+++ client-app/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+    displayName: string;
+    token: string;
+    username: string;
+    image?: string;
+}
+
+export interface IUserFormValues {
+    email: string;
+    password: string;
+    displayName?: string;
+    username?: string;
+
+}
